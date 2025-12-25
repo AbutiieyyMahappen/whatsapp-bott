b21d8c8f21ab1124ed549a615a20b9bcd9da54ac
TDX EL REY🎭 <happymahappen101@gmail.com>
2025-12-25 14:20:25 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+import makeWASocket, {
+  useMultiFileAuthState
+} from "@whiskeysockets/baileys";
+
+import readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+async function startBot() {
+  const { state, saveCreds } = await useMultiFileAuthState("auth");
+
+  const sock = makeWASocket({
+    auth: state,
+    printQRInTerminal: false
+  });
+
+  sock.ev.on("creds.update", saveCreds);
+
+  if (!state.creds.registered) {
+    rl.question("Enter WhatsApp number (with country code): ", async (number) => {
+      const code = await sock.requestPairingCode(number);
+      console.log("PAIRING CODE:", code);
+    });
+  }
+
+  sock.ev.on("messages.upsert", async ({ messages }) => {
+    const msg = messages[0];
+    if (!msg.message || msg.key.fromMe) return;
+
+    const text =
+      msg.message.conversation ||
+      msg.message.extendedTextMessage?.text;
+
+    if (text === "hi") {
+      await sock.sendMessage(msg.key.remoteJid, {
+        text: "Hello 👋 Bot connected using phone number!"
+      });
+    }
+  });
+}
+
+startBot();
